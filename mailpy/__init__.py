ddae6b9b1ffd94f51099f2d23c67e5e8e4ef2e73
Daniel Kontsek <daniel@kontsek.sk>
2015-10-17 15:41:27 +0200

    Moving towards version 0.2

--- mailpy/__init__.py
+++ mailpy/__init__.py
@@ -6,6 +6,6 @@ Copyright (c) 2015 Daniel Kontšek <daniel@kontsek.sk>
 
 License: BSD (see LICENSE for details)
 """
-__version__ = '0.1'
+__version__ = '0.2'
 __author__ = 'Daniel Kontšek'
 __license__ = 'BSD'
