147526d8197584363d2c08ce34f5ac56e0f2c105
Daniel Kontsek <daniel@kontsek.sk>
2015-10-17 15:12:12 +0200

    Added new PelicanAPI methods - Issue #3  * get_article_slugs()  * get_articles_by_slug(slug)  * get_article_by_slug(slug) Added file existence check into get_article()

--- mailpy/contrib/pelican/api.py
+++ mailpy/contrib/pelican/api.py
@@ -8,7 +8,7 @@ from pelican.settings import read_settings
 from pelican import Pelican
 
 from mailpy.contrib.git import Git, GitError
-from .exceptions import PelicanAPIError, UnknownFileFormat
+from .exceptions import PelicanAPIError, FileNotFound, MultipleFilesFound, UnknownFileFormat
 from .content import ARTICLE_CLASSES, PelicanContentFile, pelican_article
 
 __all__ = ('PelicanAPI',)
@@ -138,7 +138,12 @@ class PelicanAPI(object):
 
     def get_article(self, filename, **kwargs):
         """Return pelican article object according to filename extension"""
-        return self.article_class(self.content_path, filename, **kwargs)
+        article = self.article_class(self.content_path, filename, **kwargs)
+
+        if not article.exists():
+            raise FileNotFound(article)
+
+        return article
 
     def get_articles(self):
         """Return list of available articles filtered by file extensions"""
@@ -167,3 +172,34 @@ class PelicanAPI(object):
     def files(self):
         """Return list of files inside files folder"""
         return self.get_static_files(self.files_dir)
+
+    def get_article_slugs(self, articles=None):
+        """Return dictionary with slugs mapped to to article objects"""
+        if articles is None:
+            articles = self.articles
+
+        settings = self.settings
+
+        return {a.get_path_metadata(settings)['slug']: a for a in articles}
+
+    def get_articles_by_slug(self, slug, articles=None):
+        """Return pelican article objects according to slug name"""
+        slug_len = len(slug)
+        article_slugs = self.get_article_slugs(articles=articles)
+
+        def slug_match(x):
+            return x.startswith(slug) and (x == slug or x[slug_len:].lstrip('-').isdigit())
+
+        return [a for s, a in article_slugs.items() if slug_match(s)]
+
+    def get_article_by_slug(self, slug, articles=None):
+        """Return pelican article object according to slug name"""
+        article_list = self.get_articles_by_slug(slug, articles=articles)
+
+        if article_list:
+            if len(article_list) > 1:
+                raise MultipleFilesFound
+        else:
+            raise FileNotFound
+
+        return article_list[0]
